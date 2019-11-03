8762e6ef0ecbf3451f5e5c36dacd688c308a3589
StanStanley512 <52113355+StanStanley512@users.noreply.github.com>
2019-11-02 23:31:16 -0500

    updated text

--- config/connection.js
+++ config/connection.js
@@ -7,7 +7,7 @@ if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection({
         host: "localhost",
         user: "root",
-        password: "Lacey19$",
+        password: "password",
         database: "burgers_db"
     });
 };
