421822676df44e6793514e5d145bea4ffe8c3ffb
StanStanley512 <shane.stanleym@gmail.com>
2020-01-08 19:19:16 -0600

    updated schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,9 @@
--- Create burgers_db.
-CREATE DATABASE burgers_db;
+CREATE DATABASE IF NOT EXISTS burgers_db;
 USE burgers_db;
+
+-- If the table already exists, remove it before trying to create the table again
+DROP TABLE IF EXISTS burgers;
+
 
 -- Create burgers table
 CREATE TABLE burgers
